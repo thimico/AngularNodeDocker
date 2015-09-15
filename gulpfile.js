87b68c8ed921c483cd99d79d8188a279777cc3fe
root <root@localhost.localdomain>
2015-09-15 17:33:53 +0530

    gulp file update

--- gulpfile.js
+++ gulpfile.js
@@ -74,7 +74,6 @@ gulp.task('test', function (cb) {
     .pipe(istanbul.hookRequire()) // Force `require` to return covered files
     .on('finish', function () {
       gulp.src(['test/*.js'])
-        .pipe(mocha())
         .pipe(istanbul.writeReports()) // Creating the reports after tests ran
         .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
         .on('end', cb);
