da1abc5f0b04e812b4defc797c75f0a1434f0d84
root <root@localhost.localdomain>
2015-09-25 14:30:39 +0530

    examples folder removed

---
+++ tests/mochatest.js
@@ -0,0 +1,48 @@
+var supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where program is runninng.
+
+var server = supertest.agent("http://localhost:3033");
+
+// UNIT test begin
+
+describe("SAMPLE unit test",function(){
+
+  // #1 should return home page
+
+  it("post method",function(done){
+
+    // calling home page api
+    server
+    .post("/api/users")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      // Error key should be false.
+     // res.body.error.should.equal(false);
+      done();
+   
+    });
+  });
+
+    
+      it("get method",function(done){
+
+    // calling home page api
+    server
+    .get("/api/users")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      // Error key should be false.
+     // res.body.error.should.equal(false);
+      done();
+   
+    });
+  });
+});
